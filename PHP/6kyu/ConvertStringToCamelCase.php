fa4235ad6fc1bd3c58323b165d3a5be3c7b21b2e
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 11:38:23 +0200

    PHP 6kyu ConvertStringToCamelCase

---
+++ PHP/6kyu/ConvertStringToCamelCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 11:38
+ */
+
+function toCamelCase($str){
+    $elements = explode('@', str_replace(['-', '_'], '@', $str));
+
+    array_walk($elements, function(&$value, $key) {
+        $value = $key !== 0 ? ucfirst($value) : $value;
+    });
+
+    return implode($elements);
+}
