2cd01dc0010f427b3e9a0c175ff937c8f34dfd95
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 11:25:18 +0200

    PHP 7kyu AreTheNumbersInOrder

---
+++ PHP/7Kyu/AreTheNumbersInOrder.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 11:24
+ */
+
+function in_asc_order($arr) {
+    $prev = $arr[0];
+
+    foreach($arr as $key => $value) {
+        if($prev > $value) return false;
+        $prev = $value;
+    }
+
+    return true;
+}
