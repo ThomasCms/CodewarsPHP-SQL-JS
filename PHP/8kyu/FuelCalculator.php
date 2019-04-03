86ad4ec8910287943b206f237ac67ccfe8d51180
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 11:23:03 +0200

    PHP 8kyu FuelCalculator

---
+++ PHP/8kyu/FuelCalculator.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 11:22
+ */
+
+function fuel_price($litres, $price_per_liter) {
+    $discount = floor($litres/2) * 0.05;
+
+    if($discount > 0.25) {
+        $discount = 0.25;
+    }
+
+    return $litres * ($price_per_liter - $discount);
+}
