7b3a587e6873d51294ee130f939b01a3549d2090
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 14:11:38 +0200

    PHP 7kyu Largest5DigitNumberInASeries

---
+++ PHP/7kyu/Largest5DigitNumberInASeries.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 14:10
+ */
+
+function solution(string $s): int {
+    $max = 0;
+    for($i = 0; $i <= strlen($s) - 5; ++$i) {
+        $number = substr($s, $i, 5);
+        if($number > $max) {
+            $max = $number;
+        }
+    }
+
+    return $max;
+}
