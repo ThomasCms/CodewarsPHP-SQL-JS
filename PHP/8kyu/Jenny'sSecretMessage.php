2877be3232187d271e05841db3a5ac7cb7e80efb
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 11:00:40 +0200

    PHP 8kyu Jenny's secret message

---
+++ PHP/8kyu/Jenny'sSecretMessage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 11:00
+ */
+
+function greet($name) {
+    if ($name != 'Johnny') {
+        return "Hello, $name!";
+    }elseif ($name === 'Johnny') {
+        return 'Hello, my love!';
+    }
+}
