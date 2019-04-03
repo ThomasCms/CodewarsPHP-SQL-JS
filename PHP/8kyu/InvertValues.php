3384d10f6bb73e7e7e805409de2d20a9e10d1162
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 11:28:16 +0200

    PHP 8kyu InvertValues

---
+++ PHP/8kyu/InvertValues.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 11:28
+ */
+
+function invert(array $a): array {
+    return array_map(function ($n) {return -$n;}, $a);
+}
