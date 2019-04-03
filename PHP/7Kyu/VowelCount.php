143e495d354e5b634867003768581645d55431d5
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 11:15:23 +0200

    PHP 7kyu VowelCount

---
+++ PHP/7Kyu/VowelCount.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 11:15
+ */
+
+function getCount($str) {
+    $vowelsCount = 0;
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    $array = str_split($str);
+
+    foreach($array as $letter) {
+        if(in_array($letter, $vowels)) {
+            $vowelsCount++;
+        }
+    }
+
+    return $vowelsCount;
+}
