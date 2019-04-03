d016f8b66d8c8cce9ee73bba2f8b096b0db80c3d
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 11:13:45 +0200

    PHP 8kyu BasicMathematicalOperations

---
+++ PHP/8kyu/BasicMathematicalOperations.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 11:12
+ */
+
+function basicOp($op, $val1, $val2)
+{
+    if ($op == '+') {
+        return $val1 + $val2;
+    }
+
+    if ($op == '-') {
+        return $val1 - $val2;
+    }
+
+    if ($op == '*') {
+        return $val1 * $val2;
+    }
+
+    if($op == '/') {
+        return $val1 / $val2;
+    }
+}
