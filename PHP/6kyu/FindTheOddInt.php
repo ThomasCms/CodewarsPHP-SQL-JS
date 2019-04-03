62604d6902686638cd83efee783c5dd246e08164
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 11:27:37 +0200

    PHP 6kyu FindTheOddInt

---
+++ PHP/6kyu/FindTheOddInt.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 11:27
+ */
+
+function findIt(array $seq) : int
+{
+    $number = 0;
+
+    foreach($seq as $data){
+        $number ^= $data;
+    }
+
+    return $number;
+}
