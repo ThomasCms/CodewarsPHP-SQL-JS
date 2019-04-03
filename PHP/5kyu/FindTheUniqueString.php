7c0342b3e1fffc5dcd8da6c26e111850851fc00c
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-03 12:30:14 +0200

    PHP 5kyu FindTheUniqueString

---
+++ PHP/5kyu/FindTheUniqueString.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 03/04/19
+ * Time: 12:29
+ */
+
+function find_uniq($a) {
+    $output = [];
+    foreach ($a as $s) {
+        $duplicatesRemoved = array_unique(str_split(strtolower($s)));
+        sort($duplicatesRemoved);
+        $key = implode('', $duplicatesRemoved);
+        $output[$key][] = $s;
+    }
+    foreach ($output as $o) {
+        if (count($o) == 1) {
+            return $o[0];
+        }
+    }
+}
