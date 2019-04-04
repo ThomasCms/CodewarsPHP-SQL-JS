d7dc63201d66ecda166eb96b13ae7a05fb4d78fd
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-04 10:32:23 +0200

    PHP 7kyu FoldingYourWayToTheMoon

---
+++ PHP/7kyu/FoldingYourWayToTheMoon.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 04/04/19
+ * Time: 10:31
+ */
+
+function fold_to($distance) {
+    $num = 0.0001;
+    $result = 0;
+
+    while($num < $distance){
+        $result++;
+        $num = $num * 2;
+    }
+
+    return $distance <= 0 ? null : $result;
+}
