677bb38cff185ffb2e2aa30d39cf8497886e4fa6
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-05 09:28:23 +0200

    PHP 6kyu PersistentBugger

---
+++ PHP/6kyu/PersistentBugger.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thocou
+ * Date: 05/04/19
+ * Time: 09:27
+ */
+
+function persistence(int $num): int
+{
+    $i=0;
+
+    while($num > 9) {
+        $num = array_product(str_split($num));
+        $i++;
+    }
+
+    return $i;
+}
