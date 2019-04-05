547f749f0a565cf32becf9ce6aae932337a3ee40
Thomas Coumes <thomas.coumes@smile.fr>
2019-04-05 09:53:59 +0200

    SQL 7kyu BytesInStringFromInts

---
+++ SQL/7kyu/BytesInStringFromInts.sql
@@ -0,0 +1,7 @@
+SELECT
+length(number1::text) AS octnum1,
+length(number2::text) AS octnum2,
+length(number3::text) AS octnum3,
+length(number4::text) AS octnum4,
+length(number5::text) AS octnum5
+FROM numbers;
