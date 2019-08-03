e05e7460540789bc8abaee068460af0675bffcfb
Thomas Coumes <thomascoumes3145@gmail.com>
2019-08-03 13:16:11 +0200

    PSR fix

--- src/PHP/5kyu/WeightForWeight.php
+++ src/PHP/5kyu/WeightForWeight.php
@@ -8,19 +8,18 @@
 
 function orderWeight($str)
 {
-  $nums = explode(" ", $str);
+    $nums = explode(" ", $str);
   
-  usort($nums, function ($a, $b) {
-    $sumA = array_sum(str_split((string) $a));
-    $sumB = array_sum(str_split((string) $b));
-    
-    if ($sumA === $sumB) {
-        return strcmp($a, $b);
-    }
-    
-    return $sumA > $sumB;
-  });
-  
-  
-  return implode(' ', $nums);
+      usort($nums, function ($a, $b) {
+        $sumA = array_sum(str_split((string) $a));
+        $sumB = array_sum(str_split((string) $b));
+
+        if ($sumA === $sumB) {
+            return strcmp($a, $b);
+        }
+
+        return $sumA > $sumB;
+      });
+
+    return implode(' ', $nums);
 }
