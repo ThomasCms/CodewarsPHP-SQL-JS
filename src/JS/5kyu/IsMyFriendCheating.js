0bd6cfbdd62d729b554e20aa4ffba89b22f9d500
Thomas Coumes <thomascoumes3145@gmail.com>
2019-08-09 21:27:00 +0200

    JS 5kyu IsMyFriendCheating

---
+++ src/JS/5kyu/IsMyFriendCheating.js
@@ -0,0 +1,8 @@
+function removeNb (n) {
+    let results = [];
+    for (let a = 1; a <= n; a++) {
+        let b = (n * (n + 1) / 2 - a) / (a + 1);
+        if (b % 1 === 0 && b <= n) results.push([a, b]);
+    }
+    return results;
+}
